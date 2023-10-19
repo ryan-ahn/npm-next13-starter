bf817f79010cbff622264f0d22caef4b10001443
ryan-ahn <kaaiinn4@gmail.com>
2023-10-19 17:44:03 +0900

    Modify: 샘플 리다이렉트 미들웨어

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
+  // 샘플 리다이렉트
   if (request.nextUrl.pathname.startsWith('/redirect')) {
     return NextResponse.redirect(new URL('/', request.url));
   }
