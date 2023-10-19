b8c0cfc438fd0edd16be26ba3dbd14bfb2393126
ryan-ahn <kaaiinn4@gmail.com>
2023-10-19 16:34:48 +0900

    Modify: 글로벌 스타일 수정

--- app/_styles/global.ts
+++ app/_styles/global.ts
@@ -1,83 +1,12 @@
 import reset from 'styled-reset';
 import { createGlobalStyle } from 'styled-components';
 
-// NOTE style reset & global style
 export const GlobalStyle = createGlobalStyle`
   ${reset}
-  @keyframes clickableOnboard{
-    0%{
-      transform: scale(0.99);
-      box-shadow: 0 0 0 0 rgba(255, 221, 0, 0.9);
-    }
-    70%{
-      transform: scale(1);
-      box-shadow: 0px 0px 0 25px rgba(255, 221, 0, 0);
-    }
-    100%{
-      transform: scale(0.99);
-      box-shadow: 0 0 0 0 rgba(255, 221, 0, 0);
-    }
-  }
-  @keyframes buttonOnboard{
-    0%{
-      transform: scale(0.99);
-      box-shadow: 0 0 0 0 rgba(120, 120, 120, 0.9);
-    }
-    70%{
-      transform: scale(1);
-      box-shadow: 0px 0px 10px 18px rgba(120, 120, 120, 0);
-    }
-    100%{
-      transform: scale(0.99);
-      box-shadow: 0 0 0 0 rgba(120, 120, 120, 0);
-    }
-  }
-
-  #sleek-button{
-    left: 104px;
-    bottom: 130px;
-    z-index: 10;
-  }
   * {
     box-sizing: border-box;
     margin: 0px;
     padding: 0px;
-  }
-  body{
-    color: #343434;
-    font-family: 'Noto', sans-serif;
-    scrollbar-track-color: rgba(0, 0, 0, 0.03);
-    scrollbar-face-color: rgba(0, 0, 0, 0.2);
-    //webkit
-    ::-webkit-scrollbar {
-      width: 6px;
-    }
-    ::-webkit-scrollbar-track {
-      background: none;
-    }
-    ::-webkit-scrollbar-thumb {
-      background: rgba(0, 0, 0, 0.2);
-      border-radius: 5px;
-    }
-    ::-webkit-scrollbar-corner {
-      background: rgba(0, 0, 0, 0.5);
-    }
-    ::-webkit-scrollbar-thumb:hover {
-      background: rgba(0, 0, 0, 0.4);
-    }
-    //ms
-    ::-ms-scrollbar {
-      width: 6px;
-    }
-    ::-ms-scrollbar-track-color {
-      background: rgba(0, 0, 0, 0.03);
-    }
-    ::-ms-scrollbar-base-color {
-      background: rgba(0, 0, 0, 0.2);
-    }
-    //FireFox
-    scrollbar-width: thin;
-    scrollbar-color: rgba(0, 0, 0, 0.2) rgba(0, 0, 0, 0);
   }
   a {
     color: inherit;
@@ -87,7 +16,6 @@ export const GlobalStyle = createGlobalStyle`
     background-color: transparent;
     border: none;
     outline: none;
-    font-family: 'Noto', sans-serif;
   }
   button {
     &:active{
