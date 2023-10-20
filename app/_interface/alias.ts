5d1cf35e0354e1bdf1342f6025b255dc2d4dd81d
ryan-ahn <kaaiinn4@gmail.com>
2023-10-20 14:14:17 +0900

    Modify: 믹스인 추가

---
+++ app/_interface/alias.ts
@@ -0,0 +1,21 @@
+// Type of Flex Set
+export type TJustifyContent =
+  | 'flex-start'
+  | 'flex-end'
+  | 'center'
+  | 'space-between'
+  | 'space-around'
+  | 'space-evenly';
+
+export type TAlignItems =
+  | 'stretch'
+  | 'flex-start'
+  | 'flex-end'
+  | 'center'
+  | 'baseline';
+
+export type TFlexDirection =
+  | 'row'
+  | 'column'
+  | 'row-reverse'
+  | 'column-reverse';
---
+++ app/_styles/mixin.ts
@@ -0,0 +1,33 @@
+import { css } from 'styled-components';
+import { TAlignItems, TFlexDirection, TJustifyContent } from '@interface/alias';
+
+// flex Set
+export const flexSet = (
+  justifyContent: TJustifyContent,
+  alignItems: TAlignItems,
+  flexDirection: TFlexDirection = 'row',
+) => css`
+  display: flex;
+  flex-direction: ${flexDirection};
+  justify-content: ${justifyContent};
+  align-items: ${alignItems};
+`;
+
+// font Set
+export const fontSet = (
+  fontSize: number = 14,
+  fontWeight: number = 400,
+  lineHeight: number = 18,
+) => css`
+  font-size: ${fontSize}px;
+  font-weight: ${fontWeight};
+  line-height: ${lineHeight}px;
+`;
+
+// Background Set
+export const backgroundSet = (url: string, size: string = 'cover') => css`
+  background-image: url(${url});
+  background-size: ${size};
+  background-repeat: no-repeat;
+  background-position: center center;
+`;
