05617ac833b31be21be22a3ca00a4e28cf4d0048
ryan-ahn <kaaiinn4@gmail.com>
2023-10-22 17:27:06 +0900

    Modify: 메타 기본 설정 추가

--- app/_constants/data/meta.ts
+++ app/_constants/data/meta.ts
@@ -3,10 +3,12 @@ export const META_PAGE = {
     title: '라이언의 웹페이지',
     description: '라이언의 웹페이지입니다.',
     keywords: ['라이언', '사이트', '개인페이지', '홈'],
+    ogImage: '',
   },
   sample: {
     title: '샘플페이지',
     description: '샘플페이지 상세 정보',
     keywords: ['라이언', '사이트', '개인페이지', '샘플'],
+    ogImage: '',
   },
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,16 @@ export const metadata: Metadata = {
   title: META_PAGE.default.title,
   description: META_PAGE.default.description,
   keywords: META_PAGE.default.keywords,
+  openGraph: {
+    title: META_PAGE.default.title,
+    description: META_PAGE.default.description,
+    images: META_PAGE.default.ogImage,
+  },
+  twitter: {
+    title: META_PAGE.default.title,
+    description: META_PAGE.default.description,
+    images: META_PAGE.default.ogImage,
+  },
 };
 
 export default function rootLayout({ children }: IRootLayout) {
